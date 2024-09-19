2df6fd4935e9ab436f0385a9c8bc7f4d51acb2a2
Gavin Dean <gavin@deanfam.net>
2024-09-18 23:19:39 -0700

    Fix preview of giftdetailview

--- Gifty/GiftyApp.swift
+++ Gifty/GiftyApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @available(iOS 17, *)
 @main
 struct GiftyApp: App {
+    let modelContainer: ModelContainer
+
+        init() {
+            do {
+                modelContainer = try ModelContainer(for: Gift.self, Giftee.self, Event.self)
+            } catch {
+                fatalError("Could not initialize ModelContainer: \(error)")
+            }
+        }
     var body: some Scene {
         WindowGroup {
             MainView()
