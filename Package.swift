a0c9ef2af29fdb61129b4c34d85eef4e497292fd
Gavin Dean <gavin@deanfam.net>
2024-09-20 23:03:24 -0700

    add SPM

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Gifty",
+    platforms: [
+        .iOS(.v17),
+    ],
+    products: [
+        .library(
+            name: "Gifty",
+            targets: ["Gifty"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Gifty",
+            dependencies: []),  // Add dependencies for this target if necessary
+        .testTarget(
+            name: "GiftyTests",
+            dependencies: ["Gifty"]),
+    ]
+)
