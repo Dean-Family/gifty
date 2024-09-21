05722ad2cdc1f00e79aaeb78d4d3ba4275e9f709
Gavin Dean <gavin@deanfam.net>
2024-09-20 22:31:07 -0700

    Basic first unit test in

--- GiftyUITests/GiftyUITests.swift
+++ GiftyUITests/GiftyUITests.swift
@@ -5,34 +5,68 @@
 //  Created by Gavin Dean on 7/9/23.
 //
 
+
 import XCTest
 
 final class GiftyUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // Set up is called before each test method invocation.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Tear down is called after each test method invocation.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testTabSelection() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Test default tab is Gifts
+        XCTAssertTrue(app.tabBars.buttons["Gifts"].isSelected, "Expected the default tab to be Gifts")
+
+        // Switch to Events tab
+        app.tabBars.buttons["Events"].tap()
+        XCTAssertTrue(app.tabBars.buttons["Events"].isSelected, "Expected Events tab to be selected")
+        
+        // Switch to Giftees tab
+        app.tabBars.buttons["Giftees"].tap()
+        XCTAssertTrue(app.tabBars.buttons["Giftees"].isSelected, "Expected Giftees tab to be selected")
+    }
+    func testGiftsTabIsSelectedByDefault() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Check that the Gifts tab is selected by default
+        XCTAssertTrue(app.tabBars.buttons["Gifts"].isSelected, "Expected the Gifts tab to be selected when the app launches.")
+    }
+
+
+    func testEventsTabSelection() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Tap on Events tab
+        app.tabBars.buttons["Events"].tap()
+
+        // Check that the Events tab is selected
+        XCTAssertTrue(app.tabBars.buttons["Events"].isSelected, "Expected the Events tab to be selected.")
+    }
+
+    func testGifteesTabSelection() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Tap on Giftees tab
+        app.tabBars.buttons["Giftees"].tap()
+
+        // Check that the Giftees tab is selected
+        XCTAssertTrue(app.tabBars.buttons["Giftees"].isSelected, "Expected the Giftees tab to be selected.")
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
